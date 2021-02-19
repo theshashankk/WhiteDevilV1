e5bd36698bbe6a04e3e308f8679216ca9ba9dcdd
スタークギャング <StarkGangZ@gmail.com>
2021-02-19 16:43:47 +0530

    Update nsfw.py

--- fridaybot/modules/nsfw.py
+++ fridaybot/modules/nsfw.py
@@ -17,7 +17,6 @@ from telethon.tl.types import MessageMediaPhoto
 
 from fridaybot import CMD_HELP
 from fridaybot.utils import friday_on_cmd, sudo_cmd, admin_cmd
-from pornhub_api import PornhubApi
 from uniborg.util import friday_on_cmd
 
 
@@ -53,42 +52,6 @@ async def nsfw(event):
             await tr(event, "Response UnsucessFull. Try Again.")
             if os.path.exists(photo):
                 os.remove(photo)
-
-
-@friday.on(admin_cmd(pattern="phs (.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    api = PornhubApi()
-    data = api.search.search(
-    input_str,
-    ordering="mostviewed"
-    )
-    ok = 1
-    oik = ""
-    for vid in data.videos:
-      if ok<=5:
-        oik +=(f"""
-Video title:- {vid.title}
-Video link:- https://www.pornhub.com/view_video.php?viewkey={vid.video_id}
-
-
-
-        """)
-        ok = ok+1
-      else:
-        pass
-    
-    oiko = "<b>Links Generated Successfully</b>"+"\n"+"Search Query:- "+input_str+"\n"+oik
-    
-    await borg.send_message(
-        event.chat_id,
-        oiko,
-        parse_mode="HTML",
-    )
-    await event.delete()
-
 
 
 
